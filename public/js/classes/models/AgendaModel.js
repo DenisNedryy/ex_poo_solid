dbe3871e7ce134d2c5bf4c8e6ae1f847aba85954
Thibault <thibault.boutaud@gmx.fr>
2025-06-17 11:22:54 +0200

    model agenda premice

---
+++ public/js/classes/models/AgendaModel.js
@@ -0,0 +1,36 @@
+export class AgendaModel {
+
+    constructor() {
+
+    }
+    // recevoir les tasks, rdvs etc
+    /*
+    Object = {
+        title: null,
+        date: null,
+        type: (task, rdv, event)    
+        description: null
+    }
+    */
+
+    addTask() {
+
+    }
+
+    deleteTask() {
+
+    }
+
+    updateTask() {
+
+    }
+
+    sortTasks() {
+
+    }
+
+    searchByTitle() {
+
+    }
+
+}
